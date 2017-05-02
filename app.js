e2e05e88caa6cb84bb5711d59a1742856a44c96d
javier alvarado <jdrummer1@outlook.com>
2017-05-01 18:01:36 -0700

    Video 11

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var res = notes.addNote();
-console.log(res);
+// Using lodash module
+console.log(_.isString(true));
+console.log(_.isString('Javier'));
+
+var filteredArray = _.uniq(['Javier', 1, 'Javier', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+// var res = notes.addNote();
+// console.log(res);
 
 // My Challenge Solution
-var add = notes.add(9, -2);
-console.log(add);
+// var add = notes.add(9, -2);
+// console.log(add);
 
 // Alternative Solution
 // console.log('Result: ', notes.add(9,-2));
