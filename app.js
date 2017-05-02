1f9e11ecfcb907af9cca8e22461a9453d85757e3
javier alvarado <jdrummer1@outlook.com>
2017-05-01 18:05:16 -0700

    Video 12
    Deleted unnecessary code

--- app.js
+++ app.js
@@ -1,27 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-// Using lodash module
-console.log(_.isString(true));
-console.log(_.isString('Javier'));
-
-var filteredArray = _.uniq(['Javier', 1, 'Javier', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-// var res = notes.addNote();
-// console.log(res);
-
-// My Challenge Solution
-// var add = notes.add(9, -2);
-// console.log(add);
-
-// Alternative Solution
-// console.log('Result: ', notes.add(9,-2));
-
-// var user = os.userInfo();
-//
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
--- notes.js
+++ notes.js
@@ -1,21 +1,6 @@
 console.log('Starting notes.js');
 
-//console.log(module);
-
-//module.exports.age = 24;
-
-// Old function syntax
-// module.exports.addNote = function () {
-//
-// }
-
-// New arrow function syntax
 module.exports.addNote = () => {
   console.log('addNote');
   return 'New note';
-};
-
-// Challenge
-module.exports.add = (a,b) => {
-  return a + b;
 };
